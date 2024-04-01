588499baab29a4329aa47982edb1a3a67723a0ac
dkostadinov <d78mdd@gmail.com>
2024-04-01 17:47:22 +0300

    Add project files.

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Syntax__Conditional_Statements_and_Loops___Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
