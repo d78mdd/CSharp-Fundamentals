2cd0b03d67c981c0d0dd98fbda5a48d7420b3864
dkostadinov <d78mdd@gmail.com>
2024-04-17 23:05:05 +0300

    add a solution

--- 02.FromLeftToTheRight/Program.cs
+++ 02.FromLeftToTheRight/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _02.FromLeftToTheRight
 {
@@ -10,6 +6,41 @@ namespace _02.FromLeftToTheRight
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                int spaceIndex = input.IndexOf(' ');
+
+                string number1String = input.Substring(0, spaceIndex);
+                string number2String = input.Substring(spaceIndex + 1);
+
+                long number1 = long.Parse(number1String);
+                long number2 = long.Parse(number2String);
+
+                string numberToSumString;
+                if (number1 > number2)
+                {
+                    numberToSumString = number1String;
+                }
+                else
+                {
+                    numberToSumString = number2String;
+                }
+
+                int sum = 0;
+                foreach (char c in numberToSumString)
+                {
+                    if (c != '-')
+                    {
+                        sum += int.Parse(c.ToString());
+                    }
+                }
+
+                Console.WriteLine(sum);
+            }
         }
     }
 }
