1244af6f10601f586c04c1e745997867ae02a552
dkostadinov <d78mdd@gmail.com>
2024-04-18 19:09:49 +0300

    add solution

--- 06.BalancedBrackets/Program.cs
+++ 06.BalancedBrackets/Program.cs
@@ -6,7 +6,44 @@ namespace _06.BalancedBrackets
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
 
+            int bracketsSum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                if (input.Length == 1)
+                {
+                    if (char.Parse(input) == '(')
+                    {
+                        bracketsSum++;
+                    }
+                    if (char.Parse(input) == ')')
+                    {
+                        bracketsSum--;
+                    }
+                }
+
+                if (bracketsSum < 0)
+                {
+                    break;
+                }
+            }
+
+            string result;
+
+            if (bracketsSum == 0)
+            {
+                result = "BALANCED";
+            }
+            else
+            {
+                result = "UNBALANCED";
+            }
+
+            Console.WriteLine(result);
         }
     }
 }
